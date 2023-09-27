3395792ebfdf8d426e448d666cd64067992e7b32
santtuniskanen <santtuniska02@gmail.com>
2023-09-27 15:35:38 +0300

    finished the app

--- week_5/pageController.js
+++ week_5/pageController.js
@@ -1,9 +1,21 @@
 const pageScraper = require('./pageScraper.js');
+const fs = require('fs');
 async function scrapeAll(browserInstance){
   let browser;
   try {
     browser = await browserInstance;
-    await pageScraper.scraper(browser);
+    let scrapedData = {};
+    scrapedData['Travel'] = await pageScraper.scraper(browser, 'Travel');
+    scrapedData['HistoricalFiction'] = await pageScraper.scraper(browser, 'Historical Fiction');
+    scrapedData['Mystery'] = await pageScraper.scraper(browser, 'Mystery');
+    await browser.close();
+    fs.writeFile("data.json", JSON.stringify(scrapedData), 'utf8', function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("The data has been scraped and saved successfully! View it at './data.json'");
+    })
+    console.log(scrapedData)
   }
   catch (err) {
     console.log("Could not resolve browser instance => ", err);
